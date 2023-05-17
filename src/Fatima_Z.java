7a9b5c6e7b17cd567ffd60d04aede7679abbee6b
Asus <Asus@DESKTOP-VOS77RF>
2023-05-17 19:32:26 +0200

    2 es el numero del pato

--- src/Fatima_Z.java
+++ src/Fatima_Z.java
@@ -3,6 +3,8 @@ public class Fatima_Z {
 
 	public static void main(String[] args) {
 		System.out.println("Mi animal favorito es el gato");
+		System.out.println("Pero pienso que los patos también estan bien");
+		
 
 	}
 
