d8b3154298311042caa3b53ca1435b6f3e323064
Asus <Asus@DESKTOP-VOS77RF>
2023-05-17 19:42:10 +0200

    un 2 (pato) para la rama 2(ramaPato)

--- src/Fatima_Z.java
+++ src/Fatima_Z.java
@@ -4,6 +4,7 @@ public class Fatima_Z {
 	public static void main(String[] args) {
 		System.out.println("Mi animal favorito es el gato");
 		System.out.println("Pero pienso que los patos también estan bien");
+		System.out.println("2");
 		
 
 	}
